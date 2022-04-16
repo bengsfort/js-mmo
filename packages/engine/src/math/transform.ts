2c4e532f8b4eaab37a12f0677942185739a9f357
bengsfort <bengsfort@gmail.com>
2022-04-16 09:39:30 +0300

    start fixing transform api

--- packages/engine/src/math/transform.ts
+++ packages/engine/src/math/transform.ts
@@ -5,10 +5,27 @@ export class Transform {
   rotation: number;
   scale: Vector2;
 
+  // Global
+  private _worldPosition: Vector2;
+  private _worldRotation: number;
+  private _worldScale: Vector2;
+  // Local
+  private _localPosition: Vector2;
+  private _localRotation: number;
+  private _localScale: Vector2;
+
+  // Ownership
+  private _parent: Transform | null = null;
+  private _children: Transform[] = [];
+
   constructor(pos = new Vector2(0, 0), scale = new Vector2(1, 1), rotation = 0) {
     this.position = pos;
     this.rotation = rotation;
     this.scale = scale;
+
+    this._localPosition = pos.copy();
+    this._localRotation = rotation;
+    this._localScale = scale.copy();
   }
 
   getPositionRelativeToParent(parent: Transform): Vector2 {
